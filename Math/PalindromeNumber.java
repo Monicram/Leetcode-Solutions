60f52177e42825e6edb380434918ff7b5648eefb
Monica <monica.2301152@srec.ac.in>
2025-12-26 20:27:10 +0530

    Initial commit - LeetCode Solutions

---
+++ Math/PalindromeNumber.java
@@ -0,0 +1,17 @@
+// Palindrome Number
+// https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        int rev = 0;
+        while (temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return rev == x;
+    }
+}
